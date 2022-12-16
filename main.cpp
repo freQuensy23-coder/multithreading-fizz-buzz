72d2d30fd59ce832ac6ab4b4638a0b9dcb4e9777
Alexey <mametevalex@gmail.com>
2022-12-16 17:21:53 +0300

    [UPD] Delete todo items

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
     vector<int> tasks_balance = loadBalancer.balance(NUM_THREADS, MAX_NUM);
 
     for (int i=0; i<NUM_THREADS; i++) {
-        controllers[i] = Controller(std::make_shared<FileOutStream> ("main" + std::to_string(i) +  ".tmp"),
+        controllers[i] = Controller(std::make_shared<FileOutStream> (LoadBalancer::tmp_filename(i)),
                                     std::make_shared<IntNumber> ());
 
         std::thread t(&Controller::process_period, &controllers[i], tasks_balance[i*2], tasks_balance[i*2+1]);
@@ -30,9 +30,6 @@ int main() {
         thread.join();
     }
 
-    loadBalancer.join_result(NUM_THREADS);
-
-
-    // TODO Delete *.tmp files
+    loadBalancer.get_final_result(NUM_THREADS);
     return 0;
 }
